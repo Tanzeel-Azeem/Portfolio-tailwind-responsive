4b9376336c0e15fd72932c61a6591de007b246c2
Tanzeel-Azeem <azeemtanzeel39@gmail.com>
2024-11-05 18:31:09 -0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="text-center mt-[60px] md:ml-11 md:flex md:justify-between ">
          <div className="md:mt-40 lg:ml-20">
       <h1>Hey! {`I'm`} <br /> Tanzeel</h1>
-      <p>I'm a Front-End Developer and Ready to transform your Dreams into Reality!</p>
+      <p>{`I'm`} a Front-End Developer and Ready to transform your Dreams into Reality!</p>
      <a href="#about"><button className="px-2 py-1  bg-slate-300 hover:bg-slate-400 duration-200 text-lg  rounded-xl mt-3">Learn More</button></a> 
       <a href="#contact"><button className="px-3 py-1  bg-slate-300 hover:bg-slate-400 duration-200 text-lg ml-3 rounded-xl mt-3">Hire Me</button></a>
     </div>
